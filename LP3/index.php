89e0a0dd2028a3d92ec4527a562871209bf34793
han1967 <happyday2008dream@hotmail.com>
2018-08-16 00:51:19 +0800

    -LP3

--- LP3/index.php
+++ LP3/index.php
@@ -67,6 +67,7 @@
                                 <div class="text_holder">Safe for home use</div>
                             </li>
                             <li>
+                                <div class="img_holder"><img src="img/ico-img2.png" alt=""></div>
                                 <div class="text_holder"><span> Easy to use - 6 minutes, twice a day and you will see that it works!</span></div>
                             </li>
                             <li>
