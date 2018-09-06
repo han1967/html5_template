7a166b35cd480fead10e62b6d22f14963eab3681
han1967 <happyday2008dream@hotmail.com>
2018-09-06 19:16:39 +0800

    -css fixes Ammit suggested

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<body>
+<div class="screen-reader-response"></div>
+<form action="/2018/06/13/i-suffered-severe-pain-serious-enough-i-couldnt-work-i-discovered-device/#wpcf7-f6355-p6307-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+    <div style="display: none;">
+        <input type="hidden" name="goto" id="goto" value="">
+        <input type="hidden" name="fromWhere" id="fromWhere" value="LP">
+        <input type="hidden" name="ref" id="ref" value="">
+        <input type="hidden" name="coupon" id="coupon" value="">
+        <input type="hidden" name="game" id="game" value="">
+        <input type="hidden" name="aff_id" id="aff_id" value="">
+        <input type="hidden" name="aff_key" id="aff_key" value="">
+        <input type="hidden" name="affTranID" id="affTranID" value="">
+        <input type="hidden" name="lang" id="lang" value="">
+        <input type="hidden" name="t" id="t" value="">
+        <input type="hidden" name="r" id="r" value="">
+        <input type="hidden" name="a" id="a" value="">
+    </div>
+    <div dir="ltr">
+        <div class="custom-cf style-cf-1 clearfix" style="background:#fff; border:1px solid #360;">
+        <div class="custom-cf-title" style="text-align:left;">One of Our Experts Is Waiting for You</div>
+        <p></p>
+        <div class="cf-input-wrap clearfix">
+        <div class="cf-item">
+            <span class="wpcf7-form-control-wrap name">
+                <input type="text" name="name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="name">
+            </span>
+        </div>
+        <div class="cf-item">
+            <span class="wpcf7-form-control-wrap phone"><input type="text" name="phone" value="" size="40" maxlength="11" minlength="9" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="phone number"></span>
+        </div>
+        <div class="cf-item">
+            <span class="wpcf7-form-control-wrap email"><input type="email" name="email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="email"></span>
+        </div>
+        </div>
+        <div class="cf-button-wrap contact-green-button">
+            <input type="submit" value="Contact me for details" class="wpcf7-form-control wpcf7-submit">
+        </div>
+        <div class="cf-custom-check" style="font-weight:bold;">
+            <span class="wpcf7-form-control-wrap checkbox-97"><span class="wpcf7-form-control wpcf7-checkbox"><span class="wpcf7-list-item first last"><label><input type="checkbox" name="checkbox-97[]" value="&nbsp; &nbsp; By clicking subscribe you agree our privacy policy" checked="checked"><span class="wpcf7-list-item-label">&nbsp; &nbsp; By clicking subscribe you agree our privacy policy</span></label></span></span></span>
+        </div>
+        <p>
+        </p>
+    </div>
+</form>
+</body>
+</html>
